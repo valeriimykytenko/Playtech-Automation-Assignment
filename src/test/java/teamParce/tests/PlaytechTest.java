a95bd11da18ea83c775db90f8e7703098ec244b5
valeriimykytenko <minoga.vt@gmail.com>
2026-03-24 14:30:36 +0200

    - Added @Test hook to counting and listing Playtech teams - Added @AfterEach to ensure browser closes after tests

--- src/test/java/teamParce/tests/PlaytechTest.java
+++ src/test/java/teamParce/tests/PlaytechTest.java
@@ -1,17 +1,45 @@
 package teamParce.tests;
 
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class PlaytechTest {
     WebDriver driver;
+
     @BeforeEach
-    public void setup(){
+    public void setup() {
+        // Initializing the Chrome driver session
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
 
+    @Test
+    public void testPlaytechWebsite(){
+        // Navigate to the base URL
+        driver.get("https://www.playtechpeople.com");
+
+        //Handle Cookies
+        driver.findElement(By.id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")).click();
+
+        // TASK 2: Count and list all available teams from the main page cards
+        List<WebElement> teams = driver.findElements(By.className("team-card"));
+        System.out.println("Total Teams: " + teams.size());
+        for (WebElement team : teams) {
+            System.out.println("Team Name: " + team.getText());
+        }
+    }
 
 
+    @AfterEach
+    public void tearDown() {
+        // Ensuring the browser closes after tests
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
