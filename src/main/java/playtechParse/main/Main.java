daf0b57150f8e24b0a1f140e05b6eef4b1af3ef1
Valerii Mykytenko <minoga.vt@gmail.com>
2026-03-28 01:02:49 +0200

    optimize Main class with WebDriverManager

--- src/main/java/playtechParse/main/Main.java
+++ src/main/java/playtechParse/main/Main.java
@@ -1,46 +1,47 @@
 package playtechParse.main;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import playtechParse.pages.PlaytechSite;
+import playtechParse.pages.PlaytechWebsite;
+import playtechParse.services.WebDriverManager;
 import playtechParse.services.ReportService;
 
 import java.util.List;
 
 public class Main {
-    WebDriver driver;
-    PlaytechSite site;
-    ReportService report;
+    private PlaytechWebsite site;
+    private ReportService report;
+    private WebDriverManager driverManager ;
 
     public void setUp(){
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        driverManager = new WebDriverManager();
+        WebDriver driver = driverManager.createDriver();
         report = new ReportService();
-        site = new PlaytechSite(driver);
+        report.deleteOldReport();
+        site = new PlaytechWebsite(driver);
     }
+
+    public void tearDown(){
+        driverManager.shutDown();
+    }
+
     public void playParse(){
-
-//        site.openHome();
-        report.clearOldReport();
-
         List<String> teamNames = site.getTeamNames();
-        long teamCount = teamNames.stream().distinct().count();
-        report.printList("List of  "+teamCount+" Playtech Teams", teamNames);
-        report.saveToFile("Task 2: List of "+teamCount+" Playtech Teams", teamNames);
+        report.printList("List of " + teamNames.size() + " Playtech Teams", teamNames);
+        report.saveToFile("Task 2: List of " + teamNames.size() + " Playtech Teams", teamNames);
 
         List<String> research = site.getResearchAreas();
         report.printList("Research areas", research);
         report.saveToFile("Task 3: Research areas", research);
 
-        List<String> jobs = site.getJobsLinksByLocation("estonia");
+        List<String> jobs = site.getEstoniaJobLinks();
         report.printList("Estonia Jobs Links", jobs);
         report.saveToFile("Task 4: Jobs Links from Estonia", jobs);
-
-        driver.quit();
     }
+
     public static void main(String[] args) {
         Main app = new Main();
         app.setUp();
         app.playParse();
+        app.tearDown();
     }
 }
